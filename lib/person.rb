a270cb6be3fb5c2bff5b984d1109d09d6229248e
Araba Taylor <araba.taylor20@gmail.com>
2017-11-30 02:22:40 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,6 @@ class Person
 :weight, :handed, :complexion, :t_shirt_size,
 :wrist_size, :glove_size, :pant_length, :pant_width
   def initialization(attributes)
-    binding.pry
     attributes.each {|key, value| self.send(("#{key}="), value)}
   end
 end
